f5e1923e11f7587b7d64cab81775ddc2ad850075
Santiago Hurtado <santiago.hurtado@gmail.com>
2020-12-04 14:37:51 +0100

    feat: Driving without parameter optimization

--- src/PID.cpp
+++ src/PID.cpp
@@ -9,22 +9,21 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
-
+    this->Kp = Kp_;
+    this->Ki = Ki_;
+    this->Kd = Kd_;
+    this->p_error = 0;
+    this->i_error = 0;
+    this->d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
+     d_error = p_error;
+     i_error += cte;
+     p_error = cte;
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+    double diff = p_error -d_error;
+    return - Kp*p_error-Kd*diff-Ki*i_error;
 }
